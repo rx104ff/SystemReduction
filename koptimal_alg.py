2d51ed3827eb5ef5dfe0dcadfa78771eab61ca3f
Ningyuan Gao <ning@Ningyuans-MacBook-Air.local>
2024-12-04 16:26:32 +0900

    Finishe main algorithm

--- koptimal_alg.py
+++ koptimal_alg.py
@@ -34,12 +34,14 @@ class KOptimal:
         return True
 
     @staticmethod
-    def l_one_norm():
-        pass
+    def l_one_norm(a, b):
+        x = np.linalg.inv(-a, b)
+        return np.sum(x)
 
     @staticmethod
-    def l_two_norm():
-        pass
+    def l_two_norm(a, b):
+        x = np.linalg.inv(-a, b)
+        return np.sqrt(np.sum(x ** 2))
 
     def produce_max_optimal_at_t(self, t):
         forwarded_network = RicciFlow.compute_a_t(self.network.a, self.network.w, t)
@@ -53,3 +55,15 @@ class KOptimal:
         for k in range(self.network.n):
             if self.is_k_optimal(forwarded_network, sorted_indices[0: self.network.n - k]):
                 return k, sorted_indices[0: self.network.n - k]
+
+    def optimal_algorithm(self) -> (int, NDArray[np.int64]):
+        num_points = 100
+        t_values = np.linspace(0, 10, num_points)  # Range of t
+        current_k = self.network.n
+        ret = np.array([])
+        for t in t_values:
+            k, indices = self.produce_max_optimal_at_t(t)
+            if current_k < k:
+                ret = indices
+            else:
+                return k, ret
